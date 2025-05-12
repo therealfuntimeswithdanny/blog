cb408cf103a93051424f4395c8d369a516f58cd1
Funtimeswithdanny <142274379+therealfuntimeswithdanny@users.noreply.github.com>
2025-05-12 18:15:04 -0400

    added basic search

---
+++ assets/script/search.js
@@ -0,0 +1,56 @@
+const items = [
+    { 
+    name: "Matter’s latest update adds tap-to-pair setup via NFC", 
+    link: "/static/matter-update-with-nfc.html", 
+    description: "Two groundbreaking updates are being introduced to Matter, designed to streamline the installation process of smart home.",
+  },
+ { 
+    name: "Microsoft is revamping the Windows 11 Start menu", 
+    link: "/static/microsoft-is-revamping-the-windows-11-start-menu.html", 
+    description: "I personally pay $9.99 each month for my Office subscription, and I find it to be a fantastic price for the features and perks it provides.",
+  },
+ { 
+    name: "Google just revealed its next big Android redesign", 
+    link: "/static/is-microsoft-365-worth-it.html", 
+    description: "Found in a deleted 9to5Google post, Google accidentally revealed the next Android redesign.",
+  },
+ { 
+    name: "The Best time to Post on YouTube", 
+    link: "/static/the-best-time-to-post-on-youtube.html", 
+    description: "I'm gonna share with you the best times to post your videos to maximize views, all thanks to some awesome tips from Buffer.",
+  }, 
+  { 
+    name: "My New Best Friend", 
+    link: "/static/my-new-best-friend.html", 
+    description: "For the past couple of weeks, I've been using my Raspberry Pi 4 as my main computer. It's been a fun little experiment.",
+  },
+  { 
+    name: "New Windows start Menu", 
+    link: "/static/new-windows-start-menu.html", 
+    description: "Recently, phantomofearth (phantomofearth.bsky.social) discovered that a Windows 11 preview build reveals Microsoft's work on a redesigned Start Menu, expected to be a major improvement.",
+  },
+  { 
+    name: "Is Microsoft 365 Worth it?", 
+    link: "/static/is-microsoft-365-worth-it.html", 
+    description: "I personally pay $9.99 each month for my Office subscription, and I find it to be a fantastic price for the features and perks it provides.",
+  },
+  { 
+    name: "Fontawesome is awesome!", 
+    link: "/static/fontawesome-is-awesome.html", 
+    description: "Fontawesome is awesome and your website can be awesome with Fontawesome, now that's a lot of awesome!",
+  },
+  { 
+    name: "iPhone 14, 3 years later", 
+    link: "/static/the-iphone-13-4-years-later.html", 
+    description: "Apple announced the iPhone 14 three years ago; how does it work today!",
+  },
+  { 
+    name: "Bluesky Adds Verification via Blue Check Marks", 
+    link: "/assets/html/example-post.html", 
+    description: "Bluesky is adding Blue Check Marks for Verification of Bluesky Accounts! Hopefully I get one!",
+  },
+  { 
+    name: "Skylight Social is here a new way to watch short videos!", 
+    link: "/assets/html/example-post.html", 
+    description: "Tori (buildwithtori.com)- skylight.social is an Tik Tok like app powered by the AT Protocol!",  }
+];
