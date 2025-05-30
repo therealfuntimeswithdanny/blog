77452ecc18d06065936cc212ce22f16202336ce1
Daniel Morrisey <dannymorrisey1@outlook.com>
2025-05-30 12:58:13 +0000

    Version 10.2.1 is here

--- blog/script.js
+++ blog/script.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Define your HTML snippet as a template string
   const headerHTML = `
-<p>Version 10.2.1 Stable, <a href="https://bsky.app/profile/madebydanny.uk/post/3lqfbgnxxbs2r">View Update Notes</a></p>
+<p>Version 10.2.1 Stable, <a href="https://bsky.app/profile/madebydanny.uk/post/3lqfbuwu6ws2r">View Update Notes</a></p>
   `;
 
   // If the <nav-bar> element exists, inject the HTML into it
