67bc06066f245cd66f43acf1159ff8742abf2b55
Daniel Morrisey <dannymorrisey1@outlook.com>
2025-06-06 02:45:16 +0000

    updated js

--- neverunresolved/api/v1.js
+++ neverunresolved/api/v1.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
   var encodedUrl = encodeURIComponent(currentUrl);
   
   // Construct the redirect URL with the encoded current URL appended as a query parameter.
-  var redirectUrl = "https://madebydanny.uk/unresolved?l=" + encodedUrl;
+  var redirectUrl = "https://madebydanny.uk/resolved?l=" + encodedUrl;
   
   // Redirect the user to the new URL
   window.location.href = redirectUrl;
